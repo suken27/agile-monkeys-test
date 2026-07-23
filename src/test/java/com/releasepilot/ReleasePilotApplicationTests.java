6c8dceb5252da68836929fb53671d01952eeb806
suken27 <suken2727@gmail.com>
2026-07-23 19:03:27 +0200

    initialize springboot project and folder structure

---
+++ src/test/java/com/releasepilot/ReleasePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.releasepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasePilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
